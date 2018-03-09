b8e4361d40cdb1610043be6973752f66274a1c92
Sebastian Geisler <s.geisler@agdsn.de>
2018-03-10 00:07:07 +0100

    derive Clone and Copy trait for Error

--- src/lib.rs
+++ src/lib.rs
@@ -276,7 +276,7 @@ const CHARSET_REV: [i8; 128] = [
 const GEN: [u32; 5] = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
 
 /// Error types for Bech32 encoding / decoding
-#[derive(PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Error {
     /// String does not contain the separator character
     MissingSeparator,
