110ba6c6a314731a182117b51da595c1d3a66e37
Tobin C. Harding <me@tobin.cc>
2023-05-19 09:47:30 +1000

    Reduce noise in HTML docs
    The "overview" in `primitives::mod` causes the same info to be displayed
    twice in the HTML rendering of our docs. Just remove it.

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -2,11 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 //! Provides the internal nuts and bolts that enable bech32 encoding/decoding.
-//!
-//! ## Overview
-//!
-//! - `gf32`: GF32 elements, i.e. "bech32 characters".
-//! - `hrp`: human-readable part.
 
 pub mod gf32;
 pub mod hrp;
