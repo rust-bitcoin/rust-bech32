d432a4ffad7bdbd3d51f8febfdd0dae7746bfb10
Tobin C. Harding <me@tobin.cc>
2023-10-04 15:28:09 +1100

    Fix rendered module docs
    Currently we have docs for public modules in two places and these are
    joined together when rendered as HTML.
    
    Remove the rustdoc strings from `lib.rs` and defer to the modules for
    the module level docs.

--- src/hrp.rs
+++ src/hrp.rs
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: MIT
 
+//! Re-exports the hrp types from [`primitives::hrp`] to make importing ergonomic for the top level APIs.
+
 #[doc(inline)]
 pub use crate::primitives::hrp::{Hrp, BC, BCRT, TB};
