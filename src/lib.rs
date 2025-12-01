d477decadb8d0a63ca9fe1319201f6f07c48d9de
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-12-01 18:00:34 +0000

    fix breaking compiler changes and release 0.11.1

--- src/lib.rs
+++ src/lib.rs
@@ -127,7 +127,6 @@
 #![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
 // Experimental features we need.
 #![cfg_attr(bench, feature(test))]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 // Coding conventions
 #![deny(missing_docs)]
 
