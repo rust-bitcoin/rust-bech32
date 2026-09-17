cbc441c60ec26659bca588080d16ad2108426423
Andrew Poelstra <apoelstra@wpsoftware.net>
2026-09-17 22:01:47 +0000

    polynomial: allow generating linear-generator codes with PrintImpl
    This is a pretty dumb bug but the fix is very easy, and I suppose I
    would run into this trying to use this library for codex32 quickchecks.
    
    Fixes https://github.com/project-loupe/audit-rust-bech32/issues/38

--- src/primitives/polynomial.rs
+++ src/primitives/polynomial.rs
@@ -222,7 +222,7 @@ impl<F: Field> Polynomial<F> {
     ) -> (E, usize, ops::RangeInclusive<usize>) {
         let roots: FieldVec<usize> = self.find_nonzero_distinct_roots(E::GENERATOR).collect();
         debug_assert!(roots.len() <= self.degree());
-        // debug_assert!(roots.is_sorted()); // nightly only API
+        // debug_assert!(roots.is_sorted()); // need Rust 1.82
         assert_eq!(
             self.degree() + usize::from(self.zero_is_root()),
             roots.len(),
@@ -231,6 +231,10 @@ impl<F: Field> Polynomial<F> {
             roots,
             self.degree(),
         );
+
+        if roots.len() == 1 {
+            return (E::GENERATOR, E::MULTIPLICATIVE_ORDER, roots[0]..=roots[0]);
+        }
 
         // Brute-force (worst case n^3*log(n) in the length of the polynomial) the longest
         // geometric series within the set of roots. The common ratio between these
